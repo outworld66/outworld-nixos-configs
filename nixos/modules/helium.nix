7f98eb18e9b0e3916873da5e7da9b101ba1f4f6b
outworld66 <acidcarpet@gmail.com>
2026-09-20 23:53:54 +0300

    Install Helium inline translator declaratively

--- nixos/modules/helium.nix
+++ nixos/modules/helium.nix
@@ -1,11 +1,41 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
+
+let
+  heliumInlineTranslator = pkgs.stdenvNoCC.mkDerivation {
+    pname = "helium-inline-translator";
+    version = "1.1.1-c95e7b6";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "wesleymartinsDV";
+      repo = "helium-inline-translator";
+      rev = "c95e7b672a5b37e9c2ea98578ee6f4994071f4c5";
+      hash = "sha256-GHlmm4j0fPrMOTrcsqvUaTe6JpF2qKIa1L4Q5mx8Knk=";
+    };
+
+    postPatch = ''
+      substituteInPlace src/content.js \
+        --replace-fail 'pressedKey === "shift+alt+q"' 'pressedKey === "ctrl+shift+z"' \
+        --replace-fail 'Shift+Alt+Q' 'Ctrl+Shift+Z'
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r ./* $out/
+      runHook postInstall
+    '';
+  };
+in
 
 {
   imports = [ inputs.helium-browser.nixosModules.default ];
 
   programs.helium = {
     enable = true;
-    flags = [ "--ozone-platform-hint=auto" ];
+    flags = [
+      "--ozone-platform-hint=auto"
+      "--load-extension=${heliumInlineTranslator}"
+    ];
     policies = {
       BrowserSignin = 0;
       SpellcheckEnabled = true;
